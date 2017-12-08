d56c604be78719a3e202dbc84e36d007bca60147
gene-yoo <gene.y.yoo@gmail.com>
2017-12-08 09:32:45 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,28 @@
 export let state;
 
+export function managePets(state = { pets: [] }, action) {
+  switch (action.type) {
+    case "ADD_PET":
+      return { pets: [...state.pets, action.pet] };
 
-export function managePets(){
+    case "REMOVE_PET":
+      let updatedPets = [...state.pets].filter(pet => pet.id !== action.id);
+      return { pets: updatedPets };
+
+    default:
+      return state;
+  }
 }
 
-export function dispatch(){
+export function dispatch(action) {
+  state = managePets(state, action);
+  render();
 }
 
-export function render(){
+export function render() {
+  let text = state.pets.map(pet => {
+    return `<li>${pet.name}</li>`;
+  });
 
+  document.getElementById("container").innerHTML = `<ul>${text.join("")}</ul>`;
 }
